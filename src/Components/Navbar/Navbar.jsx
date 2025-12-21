cec82ea7899239861370a4398d2c35dbc2d900d9
Sherry <shahrzad.imani74@gmail.com>
2025-12-21 04:57:13 -0900

    Implement responsive styles in  Navbar, Hero , Footer, Contact, Campus, Testimonials, About and Program
    - Adjusted layouts for mobile and tablet
    - Improved grid alignment across components
    - Updated font sizes for better readability
    - Resized buttons and images

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import "./Navbar.css"
 import { useEffect, useState } from "react"
 import logo from "../../assets/images/logo.png"
+import menu_icon from "../../assets/images/menu-icon.png"
+import { Link } from "react-scroll";
 
 export default function Navbar () {
     const [sticky, setSticky] = useState(false);
@@ -9,17 +11,26 @@ export default function Navbar () {
       window.scrollY > 50 ? setSticky(true) : setSticky(false);
     })
   },[]);
+
+
+   const [mobileMenu, SetMobileMenu] = useState(false);
+   const toggleMenu = () => {
+      mobileMenu ? SetMobileMenu(false) : SetMobileMenu(true);
+   }
+
+
   return (
     <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
      <img src={logo} alt="" className="logo"/>
-     <ul>
-        <li>Home</li>
-        <li>Program</li>
-        <li>About us</li>
-        <li>Campus</li>
-        <li>Testimonials</li>
-        <li className="btn" ><button></button>Contact us</li>
+     <ul className={mobileMenu?'':'hide-mobile-menu'}>
+        <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
+        <li><Link to="program" smooth={true} offset={-260} duration={500}>Program</Link></li>
+        <li><Link to="about" smooth={true} offset={-150} duration={500}>About us</Link></li>
+        <li><Link to="campus" smooth={true} offset={-260} duration={500}>Campus</Link></li>
+        <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
+        <li><Link to="contact" smooth={true} offset={-250} duration={500} className="btn">Contact us</Link></li>
      </ul>
+     <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu}/>
     </nav>
   )
 }
